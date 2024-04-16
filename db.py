f407853bcea8e364a033dcbfd243b9f8ba23eece
johnsardor <sardorergashov282009@gmail.com>
2024-04-16 15:07:48 +0500

    Initial commit

---
+++ db.py
@@ -0,0 +1,86 @@
+import sqlite3
+
+db_connect = sqlite3.connect("d13.sqlite3")
+
+db_cursor = db_connect.cursor()
+
+
+def create_table_users():
+    db_cursor.execute("""
+    
+    CREATE TABLE IF NOT EXISTS users (
+    
+    id INTEGER PRIMARY KEY, full_name TEXT, phone TEXT, telegram_id INTEGER
+    
+    )
+    """)
+
+
+def create_table_product():
+    db_cursor.execute("""
+        CREATE TABLE IF NOT EXISTS product(
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        price REAL)
+    """)
+
+
+
+async def insert_user(full_name, phone, telegram_id):
+    db_cursor.execute("""
+        INSERT INTO users (full_name, phone, telegram_id)
+        VALUES(?, ?, ?)""", (full_name, phone, telegram_id))
+    db_connect.commit()
+
+
+# def alter_users():
+#     # db_cursor.execute("""
+#     # ALTER TABLE users ADD COLUMN age INTEGER DEFAULT 10""")
+#     db_cursor.execute("""
+#     ALTER TABLE users RENAME first_name to ism""")
+
+
+def insert_product(title, price):
+    db_cursor.execute("""INSERT INTO product (title, price)
+    VALUES (?,?)""", (title, price))
+
+
+def insert_orders(product_id, user_id):
+    db_cursor.execute("""INSERT INTO orders (product_id, user_id)
+    VALUES (?,?)""", (product_id, user_id))
+def read_users():
+    db_cursor.execute("""
+    SELECT * FROM users""")
+    return db_cursor
+
+def create_table_product():
+    db_cursor.execute("""
+    CREATE TABLE IF NOT EXISTS product(
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    price REAL,
+    phote TEXT)
+    """)
+
+
+async def db_insert_product(title, price, photo):
+    db_cursor.execute("""
+    INSERT INTO product (title, price, photo)
+    VALUES (?,?,?)""",(title, price, photo))
+    db_connect.commit()
+
+def get_all_products():
+    db_cursor.execute("""
+    SELECT * FROM product""")
+    return db_cursor.fetchall()
+create_table_product()
+
+def db_get_all_products():
+    products = db_cursor.execute("""
+    SELECT * FROM product""").fetchall()
+    return products
+
+
+db_connect.commit()
+# alter_users()
+db_connect.close()
